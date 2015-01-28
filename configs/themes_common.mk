d28793c1ae8fcc7af949ee04f3d7f4a9c03d1650
Michael Bestas <mikeioannina@gmail.com>
2015-01-28 02:07:06 +0200

    Remove TMobile themes permission
    * CM12 theme engine no longer supports this
    
    Change-Id: Ic1540548bf8b7b52695013cb1aae8671f737d08d
    
    Signed-off-by: STELIX <ssspinni@gmail.com>

--- configs/themes_common.mk
+++ configs/themes_common.mk
@@ -4,5 +4,4 @@ PRODUCT_PACKAGES += \
     ThemesProvider
 
 PRODUCT_COPY_FILES += \
-    vendor/aokp/prebuilt/common/etc/permissions/com.tmobile.software.themes.xml:system/etc/permissions/com.tmobile.software.themes.xml \
     vendor/aokp/prebuilt/common/etc/permissions/org.cyanogenmod.theme.xml:system/etc/permissions/org.cyanogenmod.theme.xml
