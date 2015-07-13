116819bf7fc55bd28435d0e521535cbca235087a
Adnan Begovic <adnan@cyngn.com>
2015-07-13 16:16:55 -0700

    vendor/aokp: Bump cmsdk version.
    Change-Id: Id1690423ea1754558119209390c24bfb1270920f

--- configs/common.mk
+++ configs/common.mk
@@ -273,6 +273,16 @@ PRODUCT_PROPERTY_OVERRIDES += \
 # by default, do not update the recovery with system updates
 PRODUCT_PROPERTY_OVERRIDES += persist.sys.recovery_update=false
 
+ifndef CM_PLATFORM_SDK_VERSION
+  # This is the canonical definition of the SDK version, which defines
+  # the set of APIs and functionality available in the platform.  It
+  # is a single integer that increases monotonically as updates to
+  # the SDK are released.  It should only be incremented when the APIs for
+  # the new release are frozen (so that developers don't write apps against
+  # intermediate builds).
+  CM_PLATFORM_SDK_VERSION := 2
+endif
+
 # Camera shutter sound property
 PRODUCT_PROPERTY_OVERRIDES += \
     persist.sys.camera-sound=1
