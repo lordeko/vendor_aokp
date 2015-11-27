93f47b5aec41fc6aef0d994e674a7bb4a3c120e2
lordeko <mattia.ruggeri88@gmail.com>
2015-11-27 20:22:09 +0100

    Add Hlte

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,5 +1,6 @@
 add_lunch_combo aokp_flo-userdebug
 add_lunch_combo aokp_hammerhead-userdebug
 add_lunch_combo aokp_hammerheadcaf-userdebug
+add_lunch_combo aokp_hlte-userdebug
 add_lunch_combo aokp_m8-userdebug
 add_lunch_combo aokp_oneplus2-userdebug
